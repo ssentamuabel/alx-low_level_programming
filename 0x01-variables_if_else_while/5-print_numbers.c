c7340bdf4acd00b38682a3d99bbb75a0d5209a33
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-16 13:26:29 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ *  main - print base 10 symbols
+ *
+ *  Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for(i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+
+	return (0);
+}
