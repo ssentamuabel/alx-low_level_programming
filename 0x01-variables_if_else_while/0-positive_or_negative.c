8c424ad0ce52e3b55a2be57aea3e3ecebb84323f
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-16 11:30:16 -0700

    Forth trial

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,21 +4,29 @@
 /* more headers goes here */
 
 /* betty style doc  for function main goes here */
-int main(void)
+
+/**
+ *  main - determines the sign of a random number
+ *
+ *  Return: 0 on normal occasions
+ */
+int main (void)
 {
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes here */
-	if(n > 0)
+	if (n > 0)
 	{
-		printf("%d is positive", n);
-	}else if(n == 0)
+		printf("%d is positive ", n);
+	}else if (n == 0)
 	{
-		printf("%d is zero", n);
+		printf("%d is zero ", n);
 	}else
 	{
-		printf("%d is negative", n);
+		printf("%d is negative ", n);
 	}
+
 	return (0);
 }
