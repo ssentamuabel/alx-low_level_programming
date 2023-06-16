117bea4c64dc63f211085b5b01e94c9b84892ed8
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-16 13:14:56 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+/**
+ *  main - print all letters small cap
+ *
+ *  Return: 0 when complete
+ */
+int main(void)
+{
+	char ch = 'a';
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		if (ch == 'q' || ch == 'e')
+		{
+			ch++;
+			continue;
+		}
+		putchar(ch);
+		ch++;
+	}	
+	putchar('\n');
+
+	return (0);
+}
