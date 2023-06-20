99023ce6f7fca3b77e35c8ee08951ba824b8d3e4
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-20 12:10:27 -0700

    Is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,19 +1,21 @@
 #include "main.h"
 /**
  *  _islower -checks for lower case
+ *  @c a character to check
  *
- *  Return 1 if lower else 0
+ *  Return: 1 if lower else 0
  */
 int _islower(int c)
 {
 int n;
-if (c > 64 && c < 92)
-{
-n = 0;
-}
+i
 if (c > 96 && c < 134)
 {
 n = 1;
+}
+else
+{
+n = 0;
 }
 return (n);
 }
