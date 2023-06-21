45fbf2ac44a5da03af8fd690a28194ea57a7819f
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-21 08:20:53 -0700

    fibonnacc

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+/**
+ *  main - print fibonnaci numbers
+ *
+ *  Return: 0 or success
+ */
+int main(void)
+{
+	int first = 1;
+	int last = 2;
+	int temp;
+	int i;
+
+	for (i = 1; i < 50; i++)
+	{
+		
+		printf("%d, ", first);
+		temp = first;
+		first = last;
+		last = last + temp;
+	}
+	printf("\n");
+}
