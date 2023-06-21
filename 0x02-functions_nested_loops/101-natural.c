c920540f5b495421602ad6bb5771717d8291152b
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-21 11:05:59 -0700

    Error fix

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int sum = 0;
-	int i;
+int sum = 0;
+int i;
 
-	for (i = 3; i < 1024; i++)
-	{
-		if ((i % 3 == 0) || (i % 5 == 0))
-		{
-			sum = sum + i;
-		}
-	}
-	printf("%d\n", sum);
-	
-	return (0);
+for (i = 3; i < 1024; i++)
+{
+if ((i % 3 == 0) || (i % 5 == 0))
+{
+sum = sum + i;
+}
+}
+printf("%d\n", sum);
+
+return (0);
 }
