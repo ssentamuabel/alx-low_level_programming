b40f41e1b6558337adccaf64f3c6875407c4af00
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-21 19:26:25 -0700

    space

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -14,4 +14,13 @@ void positive_or_negative(int i);
  */
 
 int largest_number(int a, int b, int c);
+/**
+* convert_day - converts day of month to day of year, without accounting
+* for leap year
+* @month: month in number format
+* @day: day of month
+* Return: day of year
+*/
+
+int convert_day(int month, int day);
 #endif /* MAIN_H */
