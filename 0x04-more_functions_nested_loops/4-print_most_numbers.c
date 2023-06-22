0c0351a68f19e33acfe0c461495c22d801c30c39
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-22 11:25:21 -0700

    Most numbers check

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
- *  print_most__numbers - print numbers less 2 and 4
+ *  print_most__numbers - print numbers less i
  *  Return: nothing
  */
-void print_most_numbers(void)
+void print_numbers(void)
 {
-	int i;
+int i;
 
-	for (i = 48; i < 58; i++)
-	{
-		if ((i == 50) || (i == 52))
-			continue;
-		_putchar((char)i);
-	}
-	_putchar((char)10);
-	
-	return;
+for (i = 48; i < 58; i++)
+{
+_putchar((char)i);
+}
+_putchar((char)10);
+
+return;
 }
