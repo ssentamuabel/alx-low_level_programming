eb7f3bf9978c5d11216fadc6fb712d404f27f12a
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-22 12:09:45 -0700

    More on numbers

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,21 +5,21 @@
  */
 void more_numbers(void)
 {
-	int values[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
-	int i, j;
-	
-	for (j = 0; j < 10; j++)
-	{
-	for (i = 0; i < 15; i++)
-	{
-		if (i > 10)
-		{
-			_putchar((char)49);
-		}
-		_putchar((char)values[i % 10]);
-	}
-	_putchar((char)10);
-	}
+int values[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
+int i, j;
 
-	return;
+for (j = 0; j < 10; j++)
+{
+for (i = 0; i < 15; i++)
+{
+if (i > 10)
+{
+_putchar((char)49);
+}
+_putchar((char)values[i % 10]);
+}
+_putchar((char)10);
+}
+
+return;
 }
