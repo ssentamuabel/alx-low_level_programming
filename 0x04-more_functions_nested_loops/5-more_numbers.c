bcbbab25e5f79f2ee159cbc6550ef71c1ddec050
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-22 12:06:12 -0700

    loop

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -3,16 +3,23 @@
  *  more_numbers - more numbers
  *  Return: null
  */
-void more_numbers(void){
+void more_numbers(void)
 {
 	int values[] = {48, 49, 50, 51, 52, 53, 54, 55, 56, 57};
 	int i, j;
-
+	
+	for (j = 0; j < 10; j++)
+	{
 	for (i = 0; i < 15; i++)
 	{
 		if (i > 10)
+		{
 			_putchar((char)49);
+		}
 		_putchar((char)values[i % 10]);
 	}
 	_putchar((char)10);
+	}
+
+	return;
 }
