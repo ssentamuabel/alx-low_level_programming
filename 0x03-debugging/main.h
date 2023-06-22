5e9de6d00d12e63a452d8b147e175b79909b4f4c
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-21 18:23:31 -0700

    largest

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -5,4 +5,13 @@
 
 void positive_or_negative(int i);
 
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
 #endif /* MAIN_H */
