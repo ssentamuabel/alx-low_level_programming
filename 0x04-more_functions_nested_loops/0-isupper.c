e45ced6beb93306e1a73ba32422f0479f8170166
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-22 11:03:06 -0700

    Is upper check

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,15 +7,15 @@
  */
 int _isupper(int c)
 {
-	int n;
-	if (c > 64 && c < 92)
-	{
-		n = 1;
-	}
-	else
-	{
-		n = 0;
-	}
+int n;
+if (c > 64 && c < 92)
+{
+n = 1;
+}
+else
+{
+n = 0;
+}
 
-	return (n);
+return (n);
 }
