1e95f375dca1288c89918bc3cf7c994048e48e1b
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-22 11:39:17 -0700

    Revert "Most numbers check"
    This reverts commit 0c0351a68f19e33acfe0c461495c22d801c30c39.

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,17 +1,19 @@
 #include "main.h"
 /**
- *  print_most__numbers - print numbers less i
+ *  print_most__numbers - print numbers less 2 and 4
  *  Return: nothing
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
-int i;
+	int i;
 
-for (i = 48; i < 58; i++)
-{
-_putchar((char)i);
-}
-_putchar((char)10);
-
-return;
+	for (i = 48; i < 58; i++)
+	{
+		if ((i == 50) || (i == 52))
+			continue;
+		_putchar((char)i);
+	}
+	_putchar((char)10);
+	
+	return;
 }
