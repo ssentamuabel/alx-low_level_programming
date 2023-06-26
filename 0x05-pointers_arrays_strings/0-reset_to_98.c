e6f1c57d5e6c66e7ff96f13c9a770fafaeb2a22f
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-26 05:49:34 -0700

    Reset

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ *  reset_to_98 - reset the input value to 98
+ *  @n: the pointer value
+ *  Return: null;
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ *  reset_to_98 - reset the input value to 98
+ *  @n: the pointer value
+ *  Return: null;
+ */
+void reset_to_98(int *n);
+#endif
