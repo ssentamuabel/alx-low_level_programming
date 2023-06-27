454a78d3e919bdad8f84ed44e65336a41a16d904
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-27 07:00:45 -0700

    len

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -8,6 +8,7 @@ int _atoi(char *s)
 	int sign = 1;
 	int result = 0;
 	int digit;
+	bool has_number = false;
 
 	while (*s != '\0')
 	{
@@ -18,9 +19,21 @@ int _atoi(char *s)
 		else if ((*s >= '0') && (*s <= '9'))
 		{
 			digit = *s - '0';
-			result = result * 10 + digit;
+		 if (result > INT_MAX / 10 || (result == INT_MAX / 10 && digit > INT_MAX % 10))
+		 {
+			 if (sign == -1)
+			 {
+				 return INT_MIN;
+			{
+			else
+			{
+				return INT_MAX;
+			}
 		}
-		else if (*s == '+')
+			result = result * 10 + digit;
+			has_number = true;
+		}
+		else if (*s == '+' && !has_number)
 		{
 		}
 		else if (result != 0)
