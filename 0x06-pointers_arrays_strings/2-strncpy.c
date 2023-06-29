c827fd21bd5ad470fa26d9512e57b7d88fcaa7c6
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-29 06:01:56 -0700

    strncpy error

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,6 +3,7 @@
  *  @dest: the destination of the copy
  *  @src: the source of the character
  *  @n: this is the number of a character
+ *  Return: address of the first element
  */
 char *_strncpy(char *dest, char *src, int n)
 {
