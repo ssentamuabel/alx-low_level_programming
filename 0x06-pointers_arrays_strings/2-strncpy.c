776484eedb2cb0340ba8db2a06706a37e5a49f8f
ssentamuabel <ssentamuabel90@gmail.com>
2023-06-29 05:59:25 -0700

    strncpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+/**
+ *  _strncpy - copy the n number of characters
+ *  @dest: the destination of the copy
+ *  @src: the source of the character
+ *  @n: this is the number of a character
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	char *temp = dest;
+       
+	while (*src != '\0' && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	while (n > 0)
+	{
+		*dest = '\0';
+		dest++;
+		n--;
+	}
+
+	return (temp);
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,7 +2,23 @@
 #define HEADER_FILE
 /**
  *  _strcat - concatenate the string
+ *  @dest: the destination string
+ *  @src: the sourse of the string
+ *  Return: address of the destination
  */
 char *_strcat(char *dest, char *src);
+/**
+ * _strncat - to concatenate the string for a given number
+ * @dest: the destination string
+ * @src: the string source
+ * @n: number of character
+ */
 char *_strncat(char *dest, char *src, int n);
+/**
+ *  _strncpy - copy the n number of characters
+ *  @dest: the destination of the copy
+ *  @src: the source of the character
+ *  @n: this is the number of a character
+ */
+char *_strncpy(char *dest, char *src, int n);
 #endif
