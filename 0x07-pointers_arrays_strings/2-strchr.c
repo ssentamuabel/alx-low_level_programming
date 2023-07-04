2941dc24d008ad2c39eb85338a6abae03cf4d074
ssentamuabel <ssentamuabel90@gmail.com>
2023-07-04 04:09:39 -0700

    Strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,17 +7,13 @@
 char *_strchr(char *s, char c)
 {
 
-	while (*s != '\0')
+	while (*s >= '\0')
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
 		s++;
-	}
-	if (*s == '\0')
-	{
-		return (s);
 	}
 
 	return (0);
