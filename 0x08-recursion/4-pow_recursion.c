61b28768ad99926e850e34c85dfafd3fb35f19ba
ssentamuabel <ssentamuabel90@gmail.com>
2023-07-06 07:45:20 -0700

    Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,30 @@
+/**
+ *  helper - help to monitor n
+ *  @x: the value to get the power
+ *  @y: the power
+ *  @n: the indexing
+ */
+int helper(int x, int y, int n)
+{
+	if (y == n)
+	{
+		return (x);
+	}
+	return (x * helper(x, y, n + 1));
+}
+/**
+ *  _pow_recursion - the power of the number
+ *
+ *  @x: the value of value
+ *  @y: the power
+ *
+ *  Return: int
+ */
+int _pow_recursion(int x, int y)
+{	int n = 0;
+	if (y < 0)
+	{
+		return (-1);
+	}
+	return (helper(x, y, n));
+}
