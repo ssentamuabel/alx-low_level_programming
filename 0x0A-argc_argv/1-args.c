3f4efdcbd1994819dfe2bbae810baaaa98ce32da
ssentamuabel <ssentamuabel90@gmail.com>
2023-07-07 07:17:19 -0700

    Arg

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ *  main - main program
+ *  @argc: the number of arguments
+ *  @argv: array of arguments
+ *
+ *  Return: int
+ */
+int main(int argc, char **argv)
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+
+	return (0);
+}
