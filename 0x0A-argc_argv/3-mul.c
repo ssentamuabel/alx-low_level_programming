2a6952075d7391d850aa66cedb5e7562fa3f0816
ssentamuabel <ssentamuabel90@gmail.com>
2023-07-07 07:55:54 -0700

    Mul

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ *  main - add two aruguments
+ *  @argc: the number of arguments
+ *  @argv: the array of arguments
+ *
+ *  Return: int
+ */
+int main(int argc, char **argv)
+{
+	if (argc < 2)
+	{
+		printf("Error\n");
+	}
+	else
+	{
+		printf("%d\n", atoi(argv[1]) *  atoi(argv[2]));
+	}
+
+
+	return (0);
+}
