01fbe90807ae970686d5bffcf9b294c7510f4459
ssentamuabel <ssentamuabel90@gmail.com>
2023-07-11 02:28:07 -0700

    first commit

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,11 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ *  create_array - creates an array of a given size
+ *  @size:the number of elements in the array
+ *  @c: the character to occupy the memory
+ *
+ *  Return: the address of the memory
+ */
+char *create_array(unsigned int size, char c);
+#endif
