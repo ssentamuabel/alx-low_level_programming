b9283ffb439eb7beec1611281947cdd019bc253c
ssentamuabel <ssentamuabel90@gmail.com>
2023-07-13 08:23:53 -0700

    First more malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include<stdlib.h>
+/**
+ *  malloc_checked - malloc check
+ *  @b: the size of memory to check
+ *
+ *  Return:the address of the pointer
+ */
+void *malloc_checked(unsigned int b)
+{
+	int *ptr = (int *)malloc(b);
+
+	if (ptr == NULL)
+		return (NULL);
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ *  malloc_checked - malloc check
+ *  @b: the size of memory to check
+ *
+ *  Return:the address of the pointer
+ */
+void *malloc_checked(unsigned int b);
+#endif
