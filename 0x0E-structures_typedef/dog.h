28afde7262f5e3aabd92374f105180a18f4324e2
ssentamuabel <ssentamuabel90@gmail.com>
2023-07-18 06:01:47 -0700

    Poppy 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ *  struct dog - a simple structur
+ *  @name: first member
+ *  @age: second member
+ *  @owner: the pointer member
+ *
+ *  Description: the is a test
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
