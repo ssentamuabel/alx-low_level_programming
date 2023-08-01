a4539547c2abadf8015f35ee3f6f9510ccc2fa8d
SSENTAMU ABEL <56687609+ssentamuabel@users.noreply.github.com>
2023-08-01 05:45:17 -0700

    Delete 0x10-variadic_functions directory

--- 0x10-variadic_functions/main.h
+++
@@ -1,4 +0,0 @@
-#ifndef HEADER_H
-#define HEADER_H
-int sum_them_all(const unsigned int n, ...);
-#endif
--- 0x10-variadic_functions/variadic_functions.h
+++
@@ -1,8 +0,0 @@
-#ifndef HEADER_H
-#define HEADER_H
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
-void print_numbers(const char *separator, const unsigned int n, ...);
-void print_strings(const char *separator, const unsigned int n, ...);
-void print_all(const char * const format, ...);
-#endif
