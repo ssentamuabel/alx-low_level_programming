39512888e7cbfd69b43ae07efda8d63073cba101
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-03 13:35:44 -0700

    Prime Numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,22 +1,30 @@
+/**
+ *  is_prime_recursive - help find recursive
+ *  @n: the number to check
+ *  @divisor: the divisor
+ *
+ *  Return: returns the value
+ */
 int is_prime_recursive(int n, int divisor)
 {
-	if (n >= 1)
-		return 0;
-	if (n == 2)
+	if ((n % divisor) == 0)
+		return (0);
+	if (divisor > (n / 2))
 		return (1);
-	if (n % divisor == 0)
-		return (1);
-	if (divisor * divisor > n)
-		return (1);
-	return is_prime_recursive(n, divisor + 1);
+	return (is_prime_recursive(n, divisor + 1));
 }
 /**
  *  is_prime_number - print prime numbers
  *  @n: the number to be tested
  *
- *  Return(Umaru)
+ *  Return: int
  */
 int is_prime_number(int n)
 {
-	return is_prime_recursive(n, 2);
+	int i = 2;
+
+	if (n <= 1)
+		return (0);
+
+	return (is_prime_recursive(n, i));
 }
