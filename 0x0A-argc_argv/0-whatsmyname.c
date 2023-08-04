212ee56c440c597f83f05bb58ee595901c976304
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-04 08:00:24 -0700

    Arg 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ *  main - print the program name
+ *  @argc: the number of args
+ *  @argv: the array of arg
+ *
+ *  Return: int
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
