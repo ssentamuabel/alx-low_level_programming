99752a285c2e261437ec237a4d49bceee78c8ff8
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-07 08:33:54 -0700

    Static one

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-find . -maxdepth 1 -type f -name "*.c" -exec gcc -c {} -o {}.o \; && ar rcs liball.a *.o
+find . -maxdepth 1 -type f -name "*.c" -exec gcc -c {} + && ar rcs liball.a *.o
 
