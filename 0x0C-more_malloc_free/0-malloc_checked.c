27712768d604c9dff5744e1521c2ca1c0d327340
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-10 03:10:03 -0700

    More malloc malloc_checked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *  malloc_checked - create space using malloc
+ *  @b: the size of bytes
+ *
+ *  Return: void
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = (void *)malloc(sizeof(b));
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* header file */
+#include<stdlib.h>
+
+/* Prototype functions */
+void *malloc_checked(unsigned int b);
+#endif
