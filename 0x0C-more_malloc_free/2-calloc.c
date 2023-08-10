3a42cb39c00fe6ee1c0ffac052cc0e043a49243f
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-10 06:33:22 -0700

    MOre malloc 2-calloc 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -13,7 +13,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(sizeof(size) * nmemb);
+	ptr = (void *)malloc(sizeof(size) * nmemb);
 	if (ptr == NULL)
 		return (NULL);
 
