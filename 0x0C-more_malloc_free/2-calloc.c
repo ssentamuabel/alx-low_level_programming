bacaacd8abfda80a40c2c53f87f386a099dfaded
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-10 06:17:50 -0700

    More malloc calloc.c 1

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  _calloc - the allocate space
+ *  @nmemb: the number of vlauses
+ *  @size: the size of the array
+ *
+ *  Return: void
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr = NULL;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	ptr = malloc(sizeof(size) * nmemb);
+	if (ptr == NULL)
+		return (NULL);
+
+	return (ptr);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,4 +7,5 @@
 /* Prototype functions */
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
