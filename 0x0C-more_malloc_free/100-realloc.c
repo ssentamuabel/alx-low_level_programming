2642954be5c2c4074cb834e53d300b58e4701287
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-11 13:18:33 -0700

    more malloc realloc 2

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -21,7 +21,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	newptr = (void *)malloc(new_size);
+	newptr = (char *)malloc(new_size);
 	if (newptr == NULL)
 		return (NULL);
 	if (ptr != NULL)
