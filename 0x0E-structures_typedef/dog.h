a1a0488e82235e4761ceb8ddeff4e85fcd4113de
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-14 06:13:20 -0700

    Struct dog 1

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ *  struct dog - struct describing a  dog
+ *  @name: pointer to the name
+ *  @age: age of the dog
+ *  @owner: pointer to the owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+}
+#endif
