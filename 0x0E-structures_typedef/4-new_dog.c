aba95b392b0ec31b6ccb9852ff1b8e270b8e7345
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-14 12:21:27 -0700

    Struct new_dog 1

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,37 @@
+#include "dog.h"
+#include<stdlib.h>
+#include<string.h>
+/**
+ *  new_dog -  to create a new dog
+ *  @name: the address of the name
+ *  @age: the age of the dog
+ *  @owner: the address of the owner
+ *
+ *  Return: pointer to the new dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *d;
+
+	d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
+		return (NULL);
+
+	d->name = strdup(name);
+	if (d->name == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->owner = strdup(owner);
+	if (d->owner == NULL)
+	{
+		free(d);
+		return (NULL);
+	}
+	d->age = age;
+
+
+	return (d);
+}
