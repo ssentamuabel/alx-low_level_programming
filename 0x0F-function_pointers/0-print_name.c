e26a7c776ed02ec30a043c510ad33075031e0acf
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-16 11:27:37 -0700

    Function Pointers print 1

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ *  print_name - prints the name
+ *  @name: the address of the name
+ *  @f: the pointer to the function
+ *
+ *  Return: null
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
