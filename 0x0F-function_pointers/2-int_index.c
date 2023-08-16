de68ec12f49ed27acbce11bcd2bf3038fbf43d12
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-16 12:36:03 -0700

    Pointer functions int_index 1

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,25 @@
+#include "function_pointers.h"
+/**
+ *  int_index - traces the index for the valid value
+ *  @array: the address of the array
+ *  @size: the size of the array
+ *  @cmp: the address of the function
+ *
+ *  Return: int
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int count = 0;
+
+	if (size < 0)
+		return (-1);
+	while (count < size)
+	{
+		if (cmp(array[count]))
+			return (count);
+		count++;
+	}
+
+	return (-1);
+
+}
