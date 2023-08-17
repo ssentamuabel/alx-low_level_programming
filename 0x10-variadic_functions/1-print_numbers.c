94082cc0d599545891c571abbecb516e70786a06
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-17 11:57:31 -0700

    another one

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,7 +19,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 		if (i != (n - 1) && separator != NULL)
 		{
-			printf("%s ", separator);
+			printf("%s", separator);
 		}
 	}
 
