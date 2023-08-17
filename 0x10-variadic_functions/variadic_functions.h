fdeb4ebb76c5071a022e313a831c02fcfe110b64
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-17 11:38:27 -0700

    Variadic functions print_number 1

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,8 +3,10 @@
 
 /* Header */
 #include<stdarg.h>
+#include<stdlib.h>
 
 /* Functions */
 int sum_them_all(const unsigned int n, ...);
 int _putchar(char c);
+void print_numbers(const char *separator, const unsigned int n, ...);
 #endif
