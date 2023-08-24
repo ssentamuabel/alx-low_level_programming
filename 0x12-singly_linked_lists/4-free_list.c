a269063dec842446398f224fc43089b0faab283b
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-24 14:51:04 -0700

    Lists free list 1

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ *  free_list - free list
+ *  @head: the pointer to head
+ *
+ *  Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = NULL;
+
+	if (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+
+		free(temp->str);
+		free(temp);
+
+	}
+}
