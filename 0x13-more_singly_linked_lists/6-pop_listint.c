3097e05823da596fd0c42fc40060d6bbfde9f66c
ssentamuabel <ssentamuabel90@gmail.com>
2023-08-29 08:25:19 -0700

    More lists 6-pop_listint

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ *  pop_listint - pop off the head
+ *  @head: the head of the list
+ *
+ *  Return: int
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *temp = *head;
+	int value = temp->n;
+
+	if (temp == NULL)
+		return (0);
+
+	temp = temp->next;
+	*head = temp;
+
+	return (value);
+}
