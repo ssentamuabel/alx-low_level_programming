3e156659d80a2d09cdd4966cca48567b43ba2475
ssentamuabel <ssentamuabel90@gmail.com>
2023-09-28 18:46:28 -0700

    bit maipulation push

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * * set_bit - setting bit at index
+ * @n: the address of the number
+ * @index: position of the bit
+ *
+ * Return: int
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int mask;
+
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
+	mask = 1UL << index;
+	*n  |= mask;
+
+	return (1);
+}
