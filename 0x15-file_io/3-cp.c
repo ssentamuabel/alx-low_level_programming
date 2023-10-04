a389710285549924b7c074593331a8f83cd41264
ssentamuabel <ssentamuabel90@gmail.com>
2023-10-04 02:52:36 -0700

    Copy files form one file to another

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -50,7 +50,7 @@ int main(int arg, char *argv[])
 		nchars = read(file_from, buff, 1024);
 		if (nchars == -1)
 			error(-1, 0, argv);
-		nwr = write(file_to, buff, 1024);
+		nwr = write(file_to, buff, nchars);
 		if (nwr == -1)
 			error(0, -1, argv);
 	}
